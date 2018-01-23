e6f873c56327da7974d2bf4e08ead25f7d45d3e6
Sridhar Ratnakumar <srid@srid.ca>
2018-01-23 15:33:06 -0500

    Add my NixOS VM

---
+++ configuration-vm.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/hardware-configuration.nix
+      ./nix/base.nix
+      ./nix/emacs.nix
+    ];
+
+  # GRUB
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.cleanTmpDir = true;
+
+  networking.hostName = "AppleDumbsUs";
+  time.timeZone = "America/New_York";
+
+  services.openssh.enable = true;
+
+  users.extraUsers.srid = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
+}
