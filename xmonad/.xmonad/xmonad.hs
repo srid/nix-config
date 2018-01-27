ab15ac5b875d8a032d18901d4dd44dd1504013e4
Sridhar Ratnakumar <srid@srid.ca>
2018-01-27 11:36:19 -0500

    xmonad: allow space for xmobar

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,11 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
 
-main = xmonad desktopConfig
+main = xmonad =<< xmobar desktopConfig
     { terminal    = "alacritty"
     , modMask     = mod4Mask
+    , layoutHook  = avoidStruts $ layoutHook desktopConfig
+    , manageHook  = manageHook desktopConfig <+> manageDocks
     }
