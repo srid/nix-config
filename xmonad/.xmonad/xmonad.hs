069d5e87af0e6bc737306db14487aca2ecb04ff4
Sridhar Ratnakumar <srid@srid.ca>
2018-02-01 05:38:50 -0500

    Xmonad border

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -6,7 +6,10 @@ import XMonad.Hooks.DynamicLog
 main = xmonad =<< xmobar desktopConfig
     { terminal    = "alacritty"
     , modMask     = mod4Mask
+    , borderWidth = 2
     -- Status bar space setup
     , layoutHook  = avoidStruts $ layoutHook desktopConfig
     , manageHook  = manageHook desktopConfig <+> manageDocks
+    , normalBorderColor = "#10b060"
+    , focusedBorderColor = "#30d080"
     }
