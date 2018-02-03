e326aae334733a5968f7d3a4c3f67e88cc9bc661
Sridhar Ratnakumar <srid@srid.ca>
2018-02-02 19:56:17 -0500

    Add nixos configuration after install

---
+++ configuration-thinkpad.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      ./nix/base.nix
+      ./nix/emacs.nix
+      ./nix/gui.nix
+      ./nix/dev.nix
+    ];
+
+  # EFI boot
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "thebeast"; # Define your hostname.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+
+  services.openssh.enable = true;
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.srid = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
+
+}
