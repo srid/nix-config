f239fa232f651fcd7e3e7b1202af049849243f3d
Sridhar Ratnakumar <srid@srid.ca>
2018-02-03 19:08:48 -0500

    Remove hardcoded screen configuration
    So that the laptop doesn't boot into blank screen when used
    standalone.
    
    Also install arandr, which we would need to manually disable
    the laptop screen when docked.

--- configuration-thinkpad.nix
+++ configuration-thinkpad.nix
@@ -39,22 +39,34 @@
     # Note: nvidia card must be enabled in BIOS.
     videoDrivers = [ "nvidia" ];
 
-    # Configuration for the LG UltraFine 5k monitor.
+    # Configuration for high res (4k/5k) monitors that use dual channel.
     # Facts:
     #  - TwinView is automatically enabled in recent nvidia drivers (no need to enable it explicitly)
     #  - nvidiaXineramaInfo must be disabled, otherwise xmonad will treat the display as two monitors.
     screenSection = ''
-      Option "MetaModes"           "DP-5: 2560x2880, DP-3: 2560x2880"
-      Option "ConnectedMonitor"    "DP-5, DP-3"
-      Option "MetaModeOrientation" "DP-3 RightOf DP-5"
       Option "nvidiaXineramaInfo"  "false"
     '';
+
+    # FIXME: To work with varied display configurations, I have to use `arandr`.
+    # For example, to manual display the laptop screen when connected to
+    # external display. This needs to be streamlined.
+
+    # If docked with my Lg Ultrafine 5k, and not using the laptop screen, I can
+    # hardcode the following configuration:
+    #  Option "MetaModes"           "DP-5: 2560x2880, DP-3: 2560x2880"
+    #  Option "ConnectedMonitor"    "DP-5, DP-3"
+    #  Option "MetaModeOrientation" "DP-3 RightOf DP-5"
+
     # Not sure if this is still required.
     serverFlagsSection = ''
       Option  "Xinerama" "0"
     '';
   };
   # TODO: xrandr switch? (autoxrandr)
+
+  environment.systemPackages = with  pkgs; [
+    arandr
+  ];
 
   # For occasions when xmonad is broken.
   services.xserver.desktopManager.plasma5.enable = true;
