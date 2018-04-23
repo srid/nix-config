9bd6150a0ca16394278423c1b68fc7421b5be6c5
Sridhar Ratnakumar <srid@srid.ca>
2018-04-23 17:14:52 -0400

    nvim: Add ghcid

--- nix/nvim/default.nix
+++ nix/nvim/default.nix
@@ -1,7 +1,19 @@
 { pkgs ? import <nixpkgs-unstable> {} }:
 
 let
+  # cf. https://nixos.wiki/wiki/Vim#Adding_new_plugins 
+
   customPlugins = {
+    neovim-ghcid = pkgs.vimUtils.buildVimPlugin {
+      name = "ghcid";
+      src = (pkgs.fetchFromGitHub { 
+        owner = "ndmitchell";
+        repo = "ghcid";
+        rev = "5d7f859bc6dd553bdf93e6453391353cf310e232";
+        sha256 = "1gyasmk6k2yqlkny27wnc1fn2khphgv400apfh1m59pzd9mdgsc2";
+      }) + "/plugins/nvim";
+    };
+
     papercolor-theme = pkgs.vimUtils.buildVimPlugin {
       name = "papercolor-theme";
       src = pkgs.fetchFromGitHub {
@@ -98,6 +110,7 @@ in
         { name = "neocomplcache"; }
         { name = "fzf-vim"; }
         { name = "fzfWrapper"; }
+        { name = "neovim-ghcid"; }
       ];
     };
   }
