74ab6d249cffdef3ce93666dc005471d9addceae
Sridhar Ratnakumar <srid@srid.ca>
2018-07-15 20:05:34 -0400

    add best screen setup

---
+++ bin/BEST.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-6 --off --output DP-5 --primary --mode 2560x2880 --pos 0x0 --rotate normal --output DP-4 --off --output DP-3 --mode 2560x2880 --pos 2560x0 --rotate normal --output DP-2 --mode 3840x2160 --pos 592x2880 --rotate normal --output DP-1 --off --output DP-0 --off
