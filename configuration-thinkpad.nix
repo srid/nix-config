49494206ff918377ba253576a361e201469c6435
Sridhar Ratnakumar <srid@srid.ca>
2018-09-23 09:50:58 -0400

    Add postgresql

--- configuration-thinkpad.nix
+++ configuration-thinkpad.nix
@@ -59,6 +59,21 @@ in
     user = "srid";
   };
 
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql100;
+    enableTCPIP = true;
+    # https://nixos.wiki/wiki/PostgreSQL
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host all all ::1/128 trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+      CREATE DATABASE nixcloud;
+      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    '';
+  };
 
   # When using just discrete graphics with nvidia, DPI is calculated
   # automatically,
