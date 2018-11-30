62c97d8bff7cb2376fc45c4d4faf84540ccc3961
Sridhar Ratnakumar <srid@srid.ca>
2018-11-29 20:58:17 -0500

    full4k layout

--- bin/full4k.sh
+++ bin/full4k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-6 --off --output DP-5 --primary --mode 4096x2304 --pos 0x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --mode 3840x2160 --pos 4096x96 --rotate normal --output DP-1 --off --output DP-0 --off
+xrandr --output DP-6 --off --output DP-5 --mode 4096x2304 --pos 0x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --mode 3840x2160 --pos 96x2408 --rotate normal --output DP-1 --off --output DP-0 --off
