203f1a13f8e046eca42b534bbaeccf4cf3f914e7
Sridhar Ratnakumar <srid@srid.ca>
2019-05-10 13:55:18 -0400

    Have pixel slate talk directly to the Thinkpad
    Had to set up port forwarding in router

--- configuration-thinkpad.nix
+++ configuration-thinkpad.nix
@@ -38,11 +38,17 @@ in
   networking.wireguard.interfaces = {
     wg0 = {
       ips = [ "10.100.0.2/24" ];
+      listenPort = 51820;
       privateKeyFile = "/home/srid/wireguard-keys/private";
       peers = [
         { publicKey = "8BLMztljWIV+9V+fXgj34GCVn0YSK6PYdAuPVkdidTs=";
-          allowedIPs = [ "10.100.0.0/24" "104.198.14.52" ];
+          allowedIPs = [ "10.100.0.1" "104.198.14.52" ];
           endpoint = "facade.srid.ca:51820";
+          persistentKeepalive = 25;
+        }
+        # pixel slate
+        { publicKey = "yMuIxno/f/eI5W+P6SsBZ0Ib5s0uhqEo/DB8MdCbryY=";
+          allowedIPs = [ "10.100.0.3" ];
           persistentKeepalive = 25;
         }
       ];
