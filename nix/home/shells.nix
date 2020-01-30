3fe58b7090814d50618144d3a6a625ee2b6329f4
Sridhar Ratnakumar <srid@srid.ca>
2020-01-30 17:24:05 -0500

    Fix ee for bash

--- nix/home/shells.nix
+++ nix/home/shells.nix
@@ -7,7 +7,7 @@ let
     copy = "${pkgs.xclip}/bin/xclip -i -selection clipboard";
     g = "git";
     e = "eval $EDITOR";
-    ee = "e (fzf)";
+    ee = "fzf | xargs $EDITOR";
     download = "aria2c --file-allocation=none --seed-time=0";
     chromecast = "castnow --address 192.168.2.64 --myip 192.168.2.76";
     gotty-sridca = "gotty -a 0.0.0.0 -p 9999 -r"; # To be run from the thebeast wireguard peer only.
