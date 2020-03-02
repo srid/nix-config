3c79c3cdd9b6aadfa7ffcfd44a12211ad7dca625
Sridhar Ratnakumar <srid@srid.ca>
2020-03-02 17:45:44 -0500

    Add ghcide-nix cache

--- nix/caches.nix
+++ nix/caches.nix
@@ -1,11 +1,15 @@
 { config, lib, pkgs, ... }:
 
 {
-   nix.binaryCaches = [
+  # NOTE: These caches are used on NixOS (nixos-rebuild) only, and not in
+  # home-manager (which would only use the user's nix.conf).
+
+  nix.binaryCaches = [
     "https://nixcache.reflex-frp.org"
     "https://srid.cachix.org/"
     "https://all-hies.cachix.org"
     "https://hercules-ci.cachix.org/"
+    "https://ghcide-nix.cachix.org/"
   ];
   # nix.trustedBinaryCaches = [
   #   "https://cachix.cachix.org"
@@ -20,5 +24,6 @@
     "srid.cachix.org-1:Vq4QePFVPaJt4Zt2SYo1he1uQVJ8pKiCer7Oyg5V6zU="
     "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
     "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+    "ghcide-nix.cachix.org-1:ibAY5FD+XWLzbLr8fxK6n8fL9zZe7jS+gYeyxyWYK5c="
   ];
 }
