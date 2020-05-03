c7c0545d855ae4d154b35a6054a8f8f1f0890c7e
Sridhar Ratnakumar <srid@srid.ca>
2020-05-03 16:36:40 -0400

    Re-install postgresql

--- machine/thinkpad/postgresql.nix
+++ machine/thinkpad/postgresql.nix
@@ -2,14 +2,17 @@
 
 {
    services.postgresql = {
-    enable = false;
-    # package = pkgs.postgresql_10;  -- not available on 18.09
-    enableTCPIP = true;
+    enable = true;
+    package = pkgs.postgresql_12;
+    enableTCPIP = false;
     # https://nixos.wiki/wiki/PostgreSQL
-    authentication = pkgs.lib.mkOverride 10 ''
+    authentication = pkgs.lib.mkOverride 10
+      ''
+      # Unix domain socket
       local all all trust
+      # TCP/IP connections from loopback only
       host all all ::1/128 trust
-    '';
+      '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
       CREATE DATABASE nixcloud;
