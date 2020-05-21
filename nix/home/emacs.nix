26e2815532b039ff5a47d06379f50e152e78d860
Sridhar Ratnakumar <srid@srid.ca>
2020-05-20 22:27:35 -0400

    Not using

--- nix/home/emacs.nix
+++ nix/home/emacs.nix
@@ -3,7 +3,7 @@
     emacsWithPackages = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages;
     # elpa vs melpa vs melpaStable: https://emacs.stackexchange.com/a/10501/2059
     myEmacs = emacsWithPackages (epkgs: [
-      epkgs.emacs-libvterm
+      # epkgs.emacs-libvterm
     ]);
   in
   {
