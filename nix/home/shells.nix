ff2715c8ecaca9ac45bf6e9110ee7bc954c8b1b4
Sridhar Ratnakumar <srid@srid.ca>
2020-07-31 17:45:51 -0400

    Add youtube-dl-audio alias

--- nix/home/shells.nix
+++ nix/home/shells.nix
@@ -10,6 +10,7 @@ let
     download = "aria2c --file-allocation=none --seed-time=0";
     # chromecast = "castnow --address 192.168.2.64 --myip 192.168.2.76";
     gotty-sridca = "gotty -a 0.0.0.0 -p 9999 -r"; # To be run from the thebeast wireguard peer only.
+    youtube-dl-audio = "youtube-dl --ignore-errors --output \"%(title)s.%(ext)s\" --extract-audio --audio-format mp3";
   };
 
 in
