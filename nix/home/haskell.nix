1176e7e34f9225bbb929fb588762eb48f64b9b48
Sridhar Ratnakumar <srid@srid.ca>
2020-08-02 21:09:10 -0400

    Get ormolu and ghcide from nixpkgs

--- nix/home/haskell.nix
+++ nix/home/haskell.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 
 let
-  ormoluSrc = pkgs.fetchFromGitHub {
-    owner = "tweag";
-    repo = "ormolu";
-    rev = "409c1f9aa8064ffc89aa8d047181ffee95e4fc46";
-    sha256 = "sha256:0j26r42nw854wn9zyn1dm1h87rjdjwfdyncjgp8fj1q19hnz99gw";
-  };
-
   # TODO: configure cache in home-manager first; until then, on macOS, use
   # 'cachix use hercules-ci' before 'home-manager switch'
   ghcideNixSrc = pkgs.fetchFromGitHub {
@@ -16,10 +9,6 @@ let
     rev = "67493b8";
     sha256 = "sha256:1zq5g7ka99vcyqbg5l1bx0rliq3ihig37nzczk0wdwidjyxjghf9";
   };
-
-  # https://github.com/haskell/cabal/issues/4739#issuecomment-359209133
-  macOSCaseNameFix = drv:
-    pkgs.haskell.lib.appendConfigureFlag drv "--ghc-option=-optP-Wno-nonportable-include-path";
 in {
   home.packages = with pkgs.haskellPackages; [
 
@@ -29,9 +18,9 @@ in {
     hlint
 
     hoogle
+    ormolu
+    ghcide
 
-    # ormolu code formatter
-    (macOSCaseNameFix (import ormoluSrc { }).ormolu)
     # ghcide
     # (import ghcideNixSrc {}).ghcide-ghc882
   ];
