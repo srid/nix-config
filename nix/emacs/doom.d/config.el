a4c8f083bc03a431e556dea368b2df348a6a3394
Sridhar Ratnakumar <srid@srid.ca>
2020-08-17 22:28:56 -0400

    typo

--- nix/emacs/doom.d/config.el
+++ nix/emacs/doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Sridhar Ratnakumat"
+(setq user-full-name "Sridhar Ratnakumar"
       user-mail-address "srid@srid.ca")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
