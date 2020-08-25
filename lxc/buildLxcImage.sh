d2052f139cce69101238ec695b6b6b2682f16feb
Sridhar Ratnakumar <srid@srid.ca>
2020-08-25 00:10:57 -0400

    Fix lxc script for new nixos-generators

--- lxc/buildLxcImage.sh
+++ lxc/buildLxcImage.sh
@@ -4,11 +4,8 @@
 set -xe
 
 CONFIGURATIONNIX=$1
-METAIMG=`nixos-generate -f lxc-metadata \
-  | xargs -r cat \
-  | awk '{print $3}'`
-IMG=`nixos-generate -c ${CONFIGURATIONNIX} -f lxc \
-  | xargs -r cat \
-  | awk '{print $3}'`
+METAIMG=`nixos-generate -f lxc-metadata`
+IMG=`nixos-generate -c ${CONFIGURATIONNIX} -f lxc`
 
+lxc image delete nixos || echo true
 lxc image import --alias nixos ${METAIMG} ${IMG}
