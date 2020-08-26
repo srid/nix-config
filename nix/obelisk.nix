2d72f18e0930d4540d99860619bd10c205e42f4c
Sridhar Ratnakumar <srid@srid.ca>
2020-08-26 18:24:01 -0400

    Refactor

--- nix/obelisk.nix
+++ nix/obelisk.nix
@@ -4,19 +4,12 @@ let
   # Dedicated user for running internet-exposed services.
   srvUser = "apps";
 
-  # TODO: drop in favour of niv
-  publicRepo = repo: rev: sha256:
-    import (pkgs.fetchFromGitHub {
-      inherit repo rev sha256;
-      owner = "srid";
-    }) {};
-
-  obeliskService = name: port: r:
+  obeliskService = name: port: obApp:
     let
-      root = pkgs.runCommand "${name}-service" {}
+      obAppWithConfig = pkgs.runCommand "${name}-service" {}
         ''
         mkdir $out
-        cp -r ${r.exe}/* $out/
+        cp -r ${obApp.exe}/* $out/
         # Copy deployment config from a private area.
         cp -r ${../private-config/deployments}/${name}/config $out/
         '';
@@ -27,8 +20,8 @@ let
       after = [ "network-online.target" ];
       environment = {};
       serviceConfig = {
-        WorkingDirectory = "${root}";
-        ExecStart = "${root}/backend -p ${port}";
+        WorkingDirectory = "${obAppWithConfig}";
+        ExecStart = "${obAppWithConfig}/backend -p ${port}";
         Restart = "on-abnormal";
         PrivateTmp = true;
         User = srvUser;
@@ -36,5 +29,5 @@ let
     };
 in
 {
-  inherit publicRepo obeliskService; 
+  inherit obeliskService; 
 }
