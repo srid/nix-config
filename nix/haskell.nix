da54ae92c894496b908e7bcf0388e12112e2e399
Sridhar Ratnakumar <srid@srid.ca>
2020-08-31 23:11:10 -0400

    cleanup

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 
-let
-  # TODO: configure cache in home-manager first; until then, on macOS, use
-  # 'cachix use hercules-ci' before 'home-manager switch'
-  ghcideNixSrc = pkgs.fetchFromGitHub {
-    owner = "cachix";
-    repo = "ghcide-nix";
-    rev = "67493b8";
-    sha256 = "sha256:1zq5g7ka99vcyqbg5l1bx0rliq3ihig37nzczk0wdwidjyxjghf9";
-  };
-in {
+{
   home.packages = with pkgs.haskellPackages; [
 
     # Some commonly used tools
@@ -20,9 +11,6 @@ in {
     hoogle
     ormolu
     ghcide
-
-    # ghcide
-    # (import ghcideNixSrc {}).ghcide-ghc882
   ];
 
   home.file = {
@@ -60,24 +48,5 @@ in {
         }
       }
       '';
-    # stylish-haskell (obsidian style)
-    # I now use ormolu; retaining this config for legacy purposes.
-    ".stylish-haskell.yaml".text = ''
-      steps:
-        - imports:
-            align: none
-            list_align: after_alias
-            long_list_align: inline
-            list_padding: 2
-            separate_lists: true
-        - language_pragmas:
-            style: vertical
-            align: false
-            remove_redundant: true
-        - trailing_whitespace: {}
-
-      columns: 110
-    '';
-
   };
 }
