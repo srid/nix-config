9d823e9f9acac0a6d73a3e879d0ee7c01eb59f4a
Sridhar Ratnakumar <srid@srid.ca>
2020-09-11 10:22:25 -0400

    Use latest kernel (no effect on 20.03)

--- nixos-configuration/x1c7.nix
+++ nixos-configuration/x1c7.nix
@@ -25,7 +25,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = hostName;
   networking.networkmanager.enable = true;
