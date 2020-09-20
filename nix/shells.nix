8bc093497a05e5e0e53d4e43d7c299b35d67896f
Sridhar Ratnakumar <srid@srid.ca>
2020-09-20 18:13:20 -0400

    chromecast

--- nix/shells.nix
+++ nix/shells.nix
@@ -11,6 +11,8 @@ let
     download = "aria2c --file-allocation=none --seed-time=0";
     gotty-sridca = "gotty -a 0.0.0.0 -p 9999 -r"; # To be run from the thebeast wireguard peer only.
     youtube-dl-audio = "youtube-dl --ignore-errors --output \"%(title)s.%(ext)s\" --extract-audio --audio-format mp3";
+    cast-video = "${pkgs.catt}/bin/catt -d SridScreen cast";
+    cast-audio = "${pkgs.catt}/bin/catt -d SridMini cast";
   };
 
 in
