39dd7854c42b428ddd6a4d04b1af2e8a8b333a89
Sridhar Ratnakumar <srid@srid.ca>
2020-10-18 15:19:39 -0400

    wg: talk to p71

--- configuration.nix/x1c7.nix
+++ configuration.nix/x1c7.nix
@@ -106,6 +106,7 @@ in {
           # bornagain
           { publicKey = "z298c6R+NXecUAEQmw/vdNMnewOT6nZ7Tx5q4Kh+5z0=";
             allowedIPs = [ "10.100.0.2" ];
+            endpoint = "192.168.2.127:51820";  # Apparently needed to be able to talk to it
           }
           # pixel slate
           #{ publicKey = "yMuIxno/f/eI5W+P6SsBZ0Ib5s0uhqEo/DB8MdCbryY=";
