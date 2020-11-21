b216ae4d51042d707abf7cc22e91823ac31fc7c8
Sridhar Ratnakumar <srid@srid.ca>
2020-11-21 00:03:48 -0500

    autorandr

--- nixos/i3.nix
+++ nixos/i3.nix
@@ -22,4 +22,10 @@
       ];
     };
   };
+
+  # Monitor plug n play
+  # https://github.com/phillipberndt/autorandr/blob/v1.0/README.md#how-to-use
+  services.autorandr = {
+    enable = true;
+  };
 }
