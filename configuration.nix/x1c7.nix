7584bb04121acdf2bcf6ef1c8ba64948884e0a80
Sridhar Ratnakumar <srid@srid.ca>
2020-11-21 19:41:06 -0500

    finalize x1c7.nix

--- configuration.nix/x1c7.nix
+++ configuration.nix/x1c7.nix
@@ -8,23 +8,29 @@ in {
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
 
+      # Get community hardware configuration
       <nixos-hardware/lenovo/thinkpad>
       <nixos-hardware/lenovo/thinkpad/x1>
       <nixos-hardware/lenovo/thinkpad/x1/7th-gen>
+      # home-manager
       <home-manager/nixos>
 
-      ../nixos/shell.nix
-      ../nixos/quebec.nix
-      ../nixos/fonts.nix
+      # Essential imports
       ../nixos/nix.nix
-      ../nixos/nix-distributed.nix
-
-      ../nixos/gnome.nix
+      ../nixos/quebec.nix
+      ../nixos/shell.nix
       ../nixos/tmux.nix
 
+      # Other imports
+      ../nixos/fonts.nix
+      ../nixos/gnome.nix
+      ../nixos/i3.nix
+      ../nixos/redshift.nix
       ../nixos/syncthing.nix
       ../nixos/syncthing-tray.nix
-      ../nixos/docker.nix
+      
+      # Remote building (because X1C7 is not the fastest)
+      ../nixos/nix-distributed.nix
 
       ../private-config/caches.nix
     ];
@@ -36,41 +42,53 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_testing;
-  
-  # Allow non-free firmware, such as for intel wifi
-  hardware = {
-    enableRedistributableFirmware = true;
-  };
-  # Sound
-  sound.enable = true;
-  # Fingerprint reader
-  services.fwupd.enable = true;
-  services.fprintd.enable = true;
-  security.pam.services.login.fprintAuth = true;
-  security.pam.services.xscreensaver.fprintAuth = true;
+  # boot.kernelPackages = pkgs.linuxPackages_testing;
+  boot.kernelPackages = let
+      customLinuxF = { fetchurl, buildLinux, ... } @ args:
+        buildLinux (args // rec {
+          version = "5.9.9";
+          extraMeta.branch = "5.9";
+          modDirVersion = "${version}";
 
-  networking = {
-    hostName = hostName;
-    networkmanager = {
-      enable = true;
-      wifi.powersave = false;
-      wifi.scanRandMacAddress = false;
-    };
-    wireless.networks = ./private-config/wifi.nix;
+          src = fetchurl {
+            url = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
+            sha256 = "1b8zysy0br131ydhc7ycxhh8d88r44xrmkf2q2lffy0jmy3d60m3";
+          };
+          kernelPatches = [];
+        } // (args.argsOverride or {}));
+      customLinux = pkgs.callPackage customLinuxF{};
+    in 
+      pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor customLinux);
 
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    useDHCP = false;
-    interfaces.wlp0s20f3.useDHCP = true;
+  # Hoping this fixes screen brightness buttons
+  # EDIT: nope, it doesn't...
+  boot.kernelParams = [
+    "acpi_backlight=vendor"
+    "video.use_native_backlight=1"
+  ];
+
+  #fileSystems."/".device = "/dev/mapper/crypted";
+
+  networking.hostName = hostName;
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  # wifi workarounds for unreliability
+  /*
+  networkmanager = {
+    enable = true;
+    wifi.powersave = false;
+    wifi.scanRandMacAddress = false;
   };
+  */
 
   # Set up Wireguard
   networking = {
-    nat = {
-      enable = true;
-    };
     firewall = {
       enable = true;
       allowedUDPPorts = [51820];
@@ -88,45 +106,40 @@ in {
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    # google-chrome UI lags on x1c7
-    # google-chrome
-    chromium
-
-    peek
-
-    mpv
-    qmmp  # winamp like
-
-    nixpkgs-master.vscode
-  ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    # Enable touchpad support.
-    libinput = {
-      enable = true;
-      naturalScrolling = true;
-    };
+  # See also nix/ssh.nix
+  programs.ssh = {
+    startAgent = true;
   };
 
-  systemd.services = {
+  programs.light.enable = true;
+
+  # Allow non-free firmware, such as for intel wifi
+  hardware = {
+    enableRedistributableFirmware = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Sound is trickly. Use alsamixer, and make sure that laptop card is not
+  # muted. Use pulsemixer to switch output device when docking/detaching.
+  sound.enable = true;
+
+  services.fwupd.enable = true;
+  # typing password is faster than fingerprint touch; leave disabled
+  #services.fprintd.enable = true;
+  #security.pam.services.login.fprintAuth = true;
+  #security.pam.services.xscreensaver.fprintAuth = true;
+
   users.users.srid = {
      isNormalUser = true;
      extraGroups = [ "wheel" "docker" ];
      shell = pkgs.bash;
      packages = with pkgs; [
        slack
+       chromium
+       vscode
+       qmmp
+       mpv
+       peek
+       pciutils
      ];
   };
 
@@ -136,5 +149,7 @@ in {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
+
 }
+
