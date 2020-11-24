5e65f57cb09c006bd3cfb9cb486a93543118334c
Sridhar Ratnakumar <srid@srid.ca>
2020-11-23 20:43:26 -0500

    fix xlock mode

--- nixos/i3.nix
+++ nixos/i3.nix
@@ -51,6 +51,10 @@ in {
       enable = true;
       time = 5; # mins
 
+      # Not sure if some modes are the cause of system freeze
+      # So deterministically pick one.
+      locker = "${pkgs.xlockmore}/bin/xlock -mode space";
+
       # Suspend after sometime (enable this after things are okay)
       # killtime = 20; # mins
       # killer = "/run/current-system/systemd/bin/systemctl suspend";
