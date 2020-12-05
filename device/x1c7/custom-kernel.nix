8445225bfb40047000577dd360223536ffb3ba94
Sridhar Ratnakumar <srid@srid.ca>
2020-12-05 16:53:36 -0500

    kernel

--- device/x1c7/custom-kernel.nix
+++ device/x1c7/custom-kernel.nix
@@ -8,13 +8,13 @@
   boot.kernelPackages = let
       customLinuxF = { fetchurl, buildLinux, ... } @ args:
         buildLinux (args // rec {
-          version = "5.9.11";
+          version = "5.9.12";
           extraMeta.branch = "5.9";
           modDirVersion = "${version}";
 
           src = fetchurl {
             url = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
-            sha256 = "0q6jlnigyjjnnxw6l724zv8acgs95s3pafabz4l9jrhhlijhmcjy";
+            sha256 = "sha256:1gfrn3sz3h4cbsf3r8f9jxja400qsmbrk8sclk4cjx1l5qcmczyr";
           };
           kernelPatches = [];
         } // (args.argsOverride or {}));
