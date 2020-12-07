c440e2f0a32d20ede3b2296d2e1706cf2b00c4d6
Sridhar Ratnakumar <srid@srid.ca>
2020-12-06 21:20:25 -0500

    cleanups

--- device/p71/hardware.nix
+++ device/p71/hardware.nix
@@ -9,16 +9,8 @@
       <nixos-hardware/common/pc/ssd>
     ];
 
-  # This machine is now a long-running home-server with a bluetooth keyboard
-  services.logind.lidSwitch = "ignore";
 
   sound.mediaKeys.enable = true;
-
-  # For Google's Titan key
-  services.udev.packages = [ 
-    pkgs.libfido2 
-    pkgs.android-udev-rules 
-  ];
 
   # Fingerprint reader
   services.fwupd.enable = true;
@@ -26,15 +18,11 @@
   security.pam.services.login.fprintAuth = true;
   security.pam.services.xscreensaver.fprintAuth = true;
 
-  # TLP Linux Advanced Power Management
-  # Seems to make suspend / wake-up work on lid-close.
   services.tlp = {
     # NOTE: disalbing tlp because I want to ignore suspend on lid. Ideally
     # disable just that. P71 is acting as a server right now, always plugged.
     enable = false;
-    settings = {
-      CPU_MAX_PERF_ON_BAT=60;
-      CPU_HWP_ON_BAT="balance_power";
-    };
   };
+  # This machine is now a long-running home-server with a bluetooth keyboard
+  services.logind.lidSwitch = "ignore";
 }
