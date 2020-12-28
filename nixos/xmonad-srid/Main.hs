7c2c4f4cb6be077c4827fe80482593bed2d17429
Sridhar Ratnakumar <srid@srid.ca>
2020-12-28 12:17:09 -0500

    xmonad border configuration

--- nixos/xmonad-srid/Main.hs
+++ nixos/xmonad-srid/Main.hs
@@ -5,7 +5,13 @@ import qualified Data.Map.Strict as M
 import XMonad
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (ToggleStruts (..), avoidStruts, docks)
+import XMonad.Layout.NoBorders (withBorder)
+import XMonad.Layout.Spacing (Border (Border), spacingRaw)
 import XMonad.Layout.ThreeColumns (ThreeCol (..))
+
+-- NOTE:
+-- Chromium won't respect Xmonad's border width, until this is done:
+-- https://wiki.archlinux.org/index.php/xmonad#Chrome/Chromium_not_displaying_defined_window_border_color
 
 main :: IO ()
 main = do
@@ -15,9 +21,15 @@ main = do
       def
         { modMask = mod4Mask, -- Use Super instead of Alt
           terminal = "myst",
-          layoutHook = avoidStruts $ ThreeColMid 1 (3 / 100) (1 / 2) ||| layoutHook def,
-          keys = myKeys
+          layoutHook =
+            borderSpacing $
+              avoidStruts $
+                ThreeColMid 1 (3 / 100) (1 / 2) ||| layoutHook def,
+          keys = myKeys,
+          -- https://htmlcolorcodes.com/
+          focusedBorderColor = "#50CBE8"
         }
+    -- Add border spacing between windows
     myKeys baseConfig@XConfig {modMask = modKey} =
       keys def baseConfig
         <> M.fromList
@@ -26,6 +38,19 @@ main = do
             ((modKey, xK_b), sendMessage ToggleStruts)
             -- ...
           ]
+    borderSpacing =
+      withBorder 5
+        . spacingRaw
+          -- Apply borders only when there are 2 or more windows
+          True
+          -- Screen border
+          (Border 0 10 10 10)
+          -- Enable screen border?
+          False
+          -- Window borders
+          (Border 10 10 10 10)
+          -- Enable window border?
+          True
 
 {- old status bar; remove after configuring taffybar
 
