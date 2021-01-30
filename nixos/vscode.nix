2899de5e929dfca5a45e74e5520dfd80b9e78817
Sridhar Ratnakumar <srid@srid.ca>
2021-01-30 14:30:51 -0500

    f

--- nixos/vscode.nix
+++ nixos/vscode.nix
@@ -16,5 +16,5 @@ in {
 
   # If xserver is not enabled, the default for this goes down. Increase it here. VSCode requires it. 
   # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
-  boot.kernel.sysctl."fs.inotify.max_user_watches" = lib.mkDefault 524288;
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
 }
