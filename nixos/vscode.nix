58191d5eb1cb60c69fe5fef52d9bfefb01c5c4c1
Sridhar Ratnakumar <srid@srid.ca>
2021-01-30 12:42:59 -0500

    vscode

--- nixos/vscode.nix
+++ nixos/vscode.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, ...}:
 
 let 
   nixpkgs-master = import ../nixos/nixpkgs-master.nix;
@@ -13,4 +13,8 @@ in {
   # For vscode to store secrets
   # Provide org.freedesktop.secrets 
   services.gnome3.gnome-keyring.enable = true;
+
+  # If xserver is not enabled, the default for this goes down. Increase it here. VSCode requires it. 
+  # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = lib.mkDefault 524288;
 }
