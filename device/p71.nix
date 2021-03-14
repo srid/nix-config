1ac33f2fed152207800c479e771e087626c1b2c8
Sridhar Ratnakumar <srid@srid.ca>
2021-03-13 20:43:27 -0500

    p71 ntfs

--- device/p71.nix
+++ device/p71.nix
@@ -63,6 +63,7 @@ in {
   # If xserver is not enabled, the default for this goes down. Increase it here. VSCode requires it. 
   # https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
   boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
+  boot.supportedFilesystems = [ "ntfs" ];
 
   hardware = {
     enableRedistributableFirmware = true;
@@ -186,6 +187,7 @@ in {
   };
 
   environment.systemPackages = with pkgs; [
+    woeusb
     signal-desktop
     tdesktop
     brave
